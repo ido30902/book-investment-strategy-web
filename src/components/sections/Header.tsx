ce3911e6eeda6f358a4773aa23caaeed2d51cb64
ido30902 <idobarabi@gmail.com>
2025-05-10 23:45:16 +0300

    changed header on hover

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -23,9 +23,11 @@ const Header = () => {
 	return (
 		<header
 			className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-				isScrolled
-					? 'bg-white/90 backdrop-blur-md shadow-sm text-transparent'
-					: 'hidden '
+				window.location.pathname === '/' 
+					? isScrolled 
+						? 'bg-white/90 backdrop-blur-md shadow-sm text-transparent' 
+						: 'hidden'
+					: 'bg-white/90 backdrop-blur-md shadow-sm text-transparent'
 			}`}>
 			<div className="container mx-auto px-4 py-4">
 				<div className="flex items-center justify-between">
